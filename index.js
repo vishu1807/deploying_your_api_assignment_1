e81f7510809c7d640ba6a418835a8d9ac9b55646
Vishwajeet Dubey <vishwajeet.dubey.s63@kalvium.community>
2025-01-24 14:35:20 +0530

    deploying

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
+// Load the dotenv package
+require('dotenv').config();
+
+// Access environment variables
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
+
+// Export the config object
+module.exports = config;
+
+// Log the variables (Optional for testing)
+console.log(config);
